12e800a12a4ccb0dfd53547d4059dd99c4d62e78
scooped8219 <119762980+scooped8219@users.noreply.github.com>
2022-12-12 11:45:32 -0600

    Add user input and console output for game result

--- myScript.js
+++ myScript.js
@@ -1,20 +1,69 @@
+//Function to generate the computer's action
 function getComputerChoice() {
   randomNumber = Math.floor(Math.random()*3);
-
-  //console.log(randomNumber);
   
-  if (randomNumber < 1) {
-  answer = 'rock';
+  if (randomNumber === 0) {
+  convertAction = 'rock';
   }
   else if (randomNumber === 1) {
-  answer = 'paper';
+  convertAction = 'paper';
   }
   else {
-  answer = 'scissors';
+  convertAction = 'scissors';
   }
-
-  //console.log(answer);
-  //console.log(randomNumber)
+  
+  return convertAction
 }
 
-//console.log(getComputerChoice());
+//Variable for computer action
+let computerChoice = getComputerChoice();
+
+//Variable for user's action
+let userChoice = prompt("rock, paper or scissors?");
+
+//Function to prompt for user
+function getUserChoice(userChoice) {
+  return userChoice;
+}
+
+//Ask User
+getUserChoice()
+//Ask Computer
+getComputerChoice()
+
+//Display game info
+console.log("Shoot!");
+console.log("Computer chose " 
++ computerChoice);
+console.log("Player chose " 
++ userChoice);
+
+if (computerChoice === userChoice) {
+  console.log("Draw!");
+  }
+else {
+  if (computerChoice === 'rock') {
+    if (userChoice === 'paper') {
+      console.log("Player wins");
+    }
+    else {
+        console.log("Computer wins");
+      }
+  }
+  else if (computerChoice === 'paper') {
+    if (userChoice === 'scissors') {
+      console.log("Player wins");
+    }
+    else {
+        console.log("Computer wins");
+      }
+  }
+  else {
+    if (userChoice === 'rock') {
+      console.log("Player wins");
+    }
+    else {
+        console.log("Computer wins");
+      }
+  }  
+}
