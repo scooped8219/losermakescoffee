4fd3030fcd22c6a913bd4d266b6ca8c90907fc18
scooped8219 <119762980+scooped8219@users.noreply.github.com>
2022-12-13 14:51:11 -0600

    Add functions to allow for multiple rounds
    From last commit several additions and mods including:
    - Initializing scores for both user and computer, and bringing to the top of the script
    - Modding getUserChoice() to force correct user entry using a while loop
    - Adding incrementers to scores for computer and user within playRound() if/else logic
    - Creating for loop to keep playing until someone reached 3 pts
    - Creating announcer(), which calls the game with an alert

--- myScript.js
+++ myScript.js
@@ -1,73 +1,106 @@
-//Function to generate the computer's action
-function getComputerChoice() {
-  randomNumber = Math.floor(Math.random()*3);
-  
-  if (randomNumber === 0) {
-  convertAction = 'rock';
+//Initialize Scoring
+let userScore = 0
+let compScore = 0
+
+function playRound() {
+  //Function to generate the computer's action
+  function getComputerChoice() {
+    randomNumber = Math.floor(Math.random()*3);
+    
+    if (randomNumber === 0) {
+    convertAction = 'rock';
+    }
+    else if (randomNumber === 1) {
+    convertAction = 'paper';
+    }
+    else {
+    convertAction = 'scissors';
+    }
+    
+    return convertAction;
   }
-  else if (randomNumber === 1) {
-  convertAction = 'paper';
+
+  //Variable for computer action
+  let computerChoice = getComputerChoice();
+
+  //Variable for user's action
+  let userChoice = getUserChoice()
+
+  //Function to get user action
+  function getUserChoice() {
+    while (true) {
+      input = prompt("Rock, paper or scissors?").toLowerCase();
+    
+    if ((input === 'rock')||(input === 'paper')||(input === 'scissors')) {
+      //console.log('Valid answer entered');
+      return input;
+      break;
+    }
+    }
   }
+
+  //Display game info
+  console.log("Shoot!");
+  console.log("Computer chose " + computerChoice);
+  console.log("Player chose " + userChoice);
+
+  //Round Evaluation
+  if (computerChoice === userChoice) {
+    console.log("Draw!");
+    }
   else {
-  convertAction = 'scissors';
+    if (computerChoice === 'rock') {
+      if (userChoice === 'paper') {
+        console.log("Player wins");
+        userScore++;    
+      }
+      else {
+          console.log("Computer wins");
+          compScore++;
+      }
+    }
+    else if (computerChoice === 'paper') {
+      if (userChoice === 'scissors') {
+        console.log("Player wins");
+        userScore++;   
+      }
+      else {
+          console.log("Computer wins");
+        compScore++;      
+      }
+    }
+    else {
+      if (userChoice === 'rock') {
+        console.log("Player wins");
+        userScore++;  
+      }
+      else {
+          console.log("Computer wins");
+        compScore++;      
+      }
+    }  
+  }
+  }
+
+//Keep playing rounds until someone gets 3 points
+for (i=0; i<100; i++) {
+  while (userScore < 3 && compScore < 3) {
+    playRound();
+    console.log("Player has " + userScore + " points");
+    console.log("Computer has " + compScore + " points");
   }
   
-  return convertAction;
-}
-
-//Variable for computer action
-let computerChoice = getComputerChoice();
-
-//Variable for user's action
-let userChoice = getUserChoice()
-
-//Function to get user action
-function getUserChoice() {
-  while (true) {
-    input = prompt("Rock, paper or scissors?").toLowerCase();
-  
-  if ((input === 'rock')||(input === 'paper')||(input === 'scissor')) {
-    console.log('Valid answer entered');
-    return input;
+  if (userScore === 3 || compScore === 3) {
+    announcer();
     break;
   }
-  }
 }
 
-//Display game info
-console.log("Shoot!");
-console.log("Computer chose " 
-+ computerChoice);
-console.log("Player chose " 
-+ userChoice);
-
-//Round Evaluation
-if (computerChoice === userChoice) {
-  console.log("Draw!");
-  }
-else {
-  if (computerChoice === 'rock') {
-    if (userChoice === 'paper') {
-      console.log("Player wins");
-    }
-    else {
-        console.log("Computer wins");
-      }
-  }
-  else if (computerChoice === 'paper') {
-    if (userChoice === 'scissors') {
-      console.log("Player wins");
-    }
-    else {
-        console.log("Computer wins");
-      }
-  }
-  else {
-    if (userChoice === 'rock') {
-      console.log("Player wins");
-    }
-    else {
-        console.log("Computer wins");
-      }
-  }  
+//Function to user's win/lose status
+function announcer() {
+  if (userScore > compScore) {
+    alert('You have won the game!');
+  } else {
+    alert('You have lost the game!');
+  } 
 }
